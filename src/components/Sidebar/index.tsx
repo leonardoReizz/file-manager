4caedc2105ba1011c93a86716a86f01f7586edea
Leonardo Reis <amarantee.reis@gmail.com>
2023-07-08 15:22:14 -0300

    change sidebar style

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -3,8 +3,6 @@ import {
   Cog6ToothIcon,
   FolderIcon,
   PresentationChartBarIcon,
-  StarIcon,
-  UserGroupIcon,
 } from "@heroicons/react/20/solid";
 import {
   Card,
@@ -16,42 +14,30 @@ import {
 
 export function Sidebar() {
   return (
-    <Card className="rounded-none p-0  left-0 h-[100vh] w-full max-w-[12rem] shadow-xl   shadow-blue-gray-900/10">
-      <div className="mb-2 p-4">
+    <Card className="rounded-none p-0  left-0 h-[100vh] w-full max-w-[13.5rem] shadow-xl   shadow-blue-gray-900/10">
+      <div className="mb-2 p-3">
         <Typography variant="h5" color="blue-gray">
-          Sidebar
+          File Manager
         </Typography>
       </div>
-      <List className="min-w-[100px] h-full text-gray-500 ">
-        <ListItem className="bg-violet-100 focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 text-violet-500 font-bold  hover:bg-violet-50 hover:text-violet-500">
+      <List className="min-w-[100px] h-full text-gray-500 text-sm">
+        <ListItem className="h-[40px] bg-violet-100 focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 text-violet-500 font-bold  hover:bg-violet-50 hover:text-violet-500">
           <ListItemPrefix>
             <PresentationChartBarIcon className="h-5 w-5" />
           </ListItemPrefix>
           All Files
         </ListItem>
-        <ListItem className="focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
+        <ListItem className="h-[40px] focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
           <ListItemPrefix>
             <FolderIcon className="h-5 w-5" />
           </ListItemPrefix>
-          Shared
+          Favorite
         </ListItem>
-        <ListItem className="focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300  active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
-          <ListItemPrefix>
-            <UserGroupIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Recent
-        </ListItem>
-        <ListItem className="focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
-          <ListItemPrefix>
-            <StarIcon className="h-5 w-5" />
-          </ListItemPrefix>
-          Settings
-        </ListItem>
-        <ListItem className="focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300  active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
+        <ListItem className="h-[40px] focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300 active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
           <ListItemPrefix>
             <Cog6ToothIcon className="h-5 w-5" />
           </ListItemPrefix>
-          Trash
+          Settings
         </ListItem>
         <ListItem className="mt-auto focus:bg-violet-300 focus:text-violet-500 active:bg-violet-300  active:text-violet-500 hover:bg-violet-50 hover:text-violet-500">
           <ListItemPrefix>
