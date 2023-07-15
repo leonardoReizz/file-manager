cb9eadd094687cae2cd6fda03eb0518117531f60
Leonardo Reis <amarantee.reis@gmail.com>
2023-07-15 14:38:04 -0300

    feat: update file list after update file

--- src/hooks/useUploadFileDialog.ts
+++ src/hooks/useUploadFileDialog.ts
@@ -1,9 +1,10 @@
-import { useMutation } from "react-query";
+import { useMutation, useQueryClient } from "react-query";
 import apiFile from "@services/http/file/index";
-import { IDefaultApiResponse } from "@services/types";
+import { DefaultApiResponse } from "@services/types";
 import { toast } from "react-toastify";
 import { useState } from "react";
 import { useFilesContext } from "./context/useFilesContext";
+import { Folder } from "@services/http/file/types";
 
 interface UseUploadFileDialogProps {
   handler: () => void;
@@ -16,6 +17,8 @@ export function useUploadFileDialog({
 }: UseUploadFileDialogProps) {
   const [selectedFile, setSelectedFile] = useState<any>(null);
   const [selectedFolder, setSelectedFolder] = useState<string>("Root");
+  const queryClient = useQueryClient();
+
   const { files } = useFilesContext();
   const handleFileChange = (event: any) => {
     setSelectedFile(event.target.files[0]);
@@ -31,8 +34,39 @@ export function useUploadFileDialog({
       formData.append("file", selectedFile as File);
       return apiFile.upload({ file: formData, folderId: selectedFolder });
     },
-    onSuccess: (response: IDefaultApiResponse) => {
+    onSuccess: (response: DefaultApiResponse) => {
+      queryClient.setQueryData<Folder[]>("manageFolders", (currentData) => {
+        if (currentData) {
+          const findIndex = currentData.findIndex(
+            (folder) => folder.folderId === response.data.message.folderId
+          );
+
+          if (findIndex >= 0) {
+            console.log(findIndex);
+            let folders = [...currentData];
+
+            folders[findIndex] = {
+              ...folders[findIndex],
+              files: [
+                ...folders[findIndex].files,
+                response.data.message.file,
+              ].sort((x, y) => {
+                const a = x.fileName.toLowerCase();
+                const b = y.fileName.toLowerCase();
+                return a === b ? 0 : a > b ? 1 : -1;
+              }),
+            };
+
+            console.log(folders);
+
+            return folders;
+          }
+          return currentData;
+        }
+        return [];
+      });
       handler();
+
       toast.success("Saved file");
     },
     onError: () => {
